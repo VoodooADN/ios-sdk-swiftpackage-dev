936be88431eb2c886434c03d97781d0b49e826c2
CI <runner@chi22-dq245-a30a8eb0-700a-41c3-8f00-87ff0e4cec30-2E620018DBC0.local>
2026-03-18 10:12:41 +0000

    Release 3.16.0-streaming4

--- Package.swift
+++ Package.swift
@@ -11,10 +11,7 @@ let package = Package(
             targets: [Constants.voodooADNName]),
         .library(
             name: Constants.OMSDKVoodooName,
-            targets: [Constants.OMSDKVoodooName]),
-        .library(
-        name: Constants.OMSDKIabtechlabName,
-        targets: [Constants.OMSDKIabtechlabName])
+            targets: [Constants.OMSDKVoodooName])
     ],
     targets: [
         .binaryTarget(
@@ -26,23 +23,15 @@ let package = Package(
             name: Constants.OMSDKVoodooName,
             url: Constants.OMSDKVoodooURL,
             checksum: Constants.OMSDKVoodooChecksum
-        ),
-        .binaryTarget(
-            name: Constants.OMSDKIabtechlabName,
-            url: Constants.OMSDKIabtechlabURL,
-            checksum: Constants.OMSDKIabtechlabChecksum
         )
     ]
 )
 
 enum Constants {
-    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.15.6-OMID3/VoodooAdn.zip"}
-    static var voodooADNChecksum: String { "7fea4a713639eeddf62c749aeee4bf6d7f8a8bbd90e8dc973ba9f3f62130f6c5" }
+    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.0-streaming4/VoodooAdn.zip"}
+    static var voodooADNChecksum: String { "d240bee355948a7b83cc3091fbff9bebd4bbcb2626140d41c8c32941df841034" }
     static var voodooADNName: String { "VoodooAdn" }
     static var OMSDKVoodooURL: String { "https://framework.voodoo-adn.com/omsdk/ios/1.6.1/OMSDK_Voodooio.zip"}
     static var OMSDKVoodooChecksum: String { "5a6b2c676db6ee8cc9d7afc320bc16a10268c65bf27987ab2c60dd4fbc5a141e" }
     static var OMSDKVoodooName: String { "OMSDK_Voodooio" }
-    static var OMSDKIabtechlabURL: String { "https://framework.voodoo-adn-dev.com/omsdk/ios/1.6.4-IAB/OMSDK_Iabtechlab.zip" }
-    static var OMSDKIabtechlabChecksum: String { "9349cea38345927ce6a6936f6f78af274e3e0c2a95e2d80fb03df67e35fd02b4" }
-    static var OMSDKIabtechlabName: String { "OMSDK_Iabtechlab" }
 }
