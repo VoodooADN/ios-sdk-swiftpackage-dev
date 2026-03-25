a15405b06e9f0086c1c5193682704ef43933db60
CI <runner@dsm12-be224-2b92db74-83f2-46b4-9352-c7d8523f5426-825A8AE17C0E.local>
2026-03-25 14:36:14 +0000

    Release 3.16.0-ADXP1710.10

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
 )
 
 enum Constants {
-    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.0-ADXP1710.9/VoodooAdn.zip"}
-    static var voodooADNChecksum: String { "84220c51a067f46197d4466fcfdeaa68f85e96c035ae824b4aa4c7ecf0a962e3" }
+    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.0-ADXP1710.10/VoodooAdn.zip"}
+    static var voodooADNChecksum: String { "5e0f72be3296b172bfa2963c5ddeac6982f25a99ca26c308aa357c5e6d9f1bfe" }
     static var voodooADNName: String { "VoodooAdn" }
     static var OMSDKVoodooURL: String { "https://framework.voodoo-adn.com/omsdk/ios/1.6.1/OMSDK_Voodooio.zip"}
     static var OMSDKVoodooChecksum: String { "5a6b2c676db6ee8cc9d7afc320bc16a10268c65bf27987ab2c60dd4fbc5a141e" }
