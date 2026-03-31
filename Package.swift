877065239b93231344b9b478ee0e2d131adaff8d
CI <runner@chi22-dq252-270e67b5-e301-465c-a91e-42e12600deeb-7621D203CDFD.local>
2026-03-31 15:26:49 +0000

    Release 3.16.0-BlackScreenFix

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
 )
 
 enum Constants {
-    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.0-BlackScreen/VoodooAdn.zip"}
-    static var voodooADNChecksum: String { "55465f5875db55c035a079e209d0cc34d7b441aaa30f31ad739d355901b163a7" }
+    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.0-BlackScreenFix/VoodooAdn.zip"}
+    static var voodooADNChecksum: String { "ca0cde12abe355b469621cc40a824033a92c5ab692d44fe1ce7ff5265a4cb5a2" }
     static var voodooADNName: String { "VoodooAdn" }
     static var OMSDKVoodooURL: String { "https://framework.voodoo-adn.com/omsdk/ios/1.6.1/OMSDK_Voodooio.zip"}
     static var OMSDKVoodooChecksum: String { "5a6b2c676db6ee8cc9d7afc320bc16a10268c65bf27987ab2c60dd4fbc5a141e" }
