2409f9baf7b7cb49880aaa7278e2575e2d697aeb
CI <runner@dsm12-be220-d9f8b15d-7820-4ea5-a367-7eb36892c435-76097A66562A.local>
2026-04-23 15:55:10 +0000

    Release 3.17.0-FixiPad2

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
 )
 
 enum Constants {
-    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.16.1-BR2/VoodooAdn.zip"}
-    static var voodooADNChecksum: String { "092759cbf58175091817a6fd7c3d221ef61ef4857c4c8879743d58b9ba31de95" }
+    static var voodooADNURL: String { "https://framework.voodoo-adn-dev.com/iOS/sdk/3.17.0-FixiPad2/VoodooAdn.zip"}
+    static var voodooADNChecksum: String { "b0b599a50bcb53613ce8c867cfe163d11873f9396a302b845e0f5e6e7526be24" }
     static var voodooADNName: String { "VoodooAdn" }
     static var OMSDKVoodooURL: String { "https://framework.voodoo-adn.com/omsdk/ios/1.6.1/OMSDK_Voodooio.zip"}
     static var OMSDKVoodooChecksum: String { "5a6b2c676db6ee8cc9d7afc320bc16a10268c65bf27987ab2c60dd4fbc5a141e" }
